325e14d1b2542efd5c6c91c51cf34dfc12a412da
Younger <youngerliu@gmail.com>
2019-06-20 14:54:14 +0800

    typo

--- Problem Sets/Problem Set 2/student_func.cu
+++ Problem Sets/Problem Set 2/student_func.cu
@@ -298,7 +298,7 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
   //TODO: Call your convolution kernel here 3 times, once for each color channel.
   gaussian_blur<<<gridSize, blockSize>>>(d_red, d_redBlurred, numRows, numCols, d_filter, filterWidth);
   gaussian_blur<<<gridSize, blockSize>>>(d_green, d_greenBlurred, numRows, numCols, d_filter, filterWidth);
-  gaussian_blur<<<gridSize, blockSize>>>(d_blueB, d_greenBlurred, numRows, numCols, d_filter, filterWidth);                                 
+  gaussian_blur<<<gridSize, blockSize>>>(d_blue, d_greenBlurred, numRows, numCols, d_filter, filterWidth);                                 
   // Again, call cudaDeviceSynchronize(), then call checkCudaErrors() immediately after
   // launching your kernel to make sure that you didn't make any mistakes.
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
