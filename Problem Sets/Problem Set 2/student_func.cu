e954764febe8e628cb22ed760cfe4c5628412dba
Younger <youngerliu@gmail.com>
2019-06-20 16:26:40 +0800

    fix wrong kernel paremeter for blue layer and add sync after kernel

--- Problem Sets/Problem Set 2/student_func.cu
+++ Problem Sets/Problem Set 2/student_func.cu
@@ -187,7 +187,7 @@ void separateChannels(const uchar4* const inputImageRGBA,
   // {
   //     return;
   // }
-  if (absolute_image_position.x > numCols || absolute_image_position.y > numRows) {
+  if (absolute_image_position.x >= numCols || absolute_image_position.y >= numRows) {
     return;
   }
 
@@ -291,8 +291,10 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
 
   //TODO: Call your convolution kernel here 3 times, once for each color channel.
   gaussian_blur<<<gridSize, blockSize>>>(d_red, d_redBlurred, numRows, numCols, d_filter, filterWidth);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
   gaussian_blur<<<gridSize, blockSize>>>(d_green, d_greenBlurred, numRows, numCols, d_filter, filterWidth);
-  gaussian_blur<<<gridSize, blockSize>>>(d_blue, d_greenBlurred, numRows, numCols, d_filter, filterWidth);                                 
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  gaussian_blur<<<gridSize, blockSize>>>(d_blue, d_blueBlurred, numRows, numCols, d_filter, filterWidth);                                 
   // Again, call cudaDeviceSynchronize(), then call checkCudaErrors() immediately after
   // launching your kernel to make sure that you didn't make any mistakes.
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
