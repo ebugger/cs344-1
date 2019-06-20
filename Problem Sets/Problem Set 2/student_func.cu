b61ab032656518ba31ab64e736a0ee9b431b4b93
Younger <youngerliu@gmail.com>
2019-06-20 15:16:13 +0800

    rewrite kernel with min/max

--- Problem Sets/Problem Set 2/student_func.cu
+++ Problem Sets/Problem Set 2/student_func.cu
@@ -129,8 +129,9 @@ void gaussian_blur(const unsigned char* const inputChannel,
     return;
   }
   float result = 0.f;
-  for (int filter_r = -filterWidth/2; filter_r <= filterWidth/2; ++filter_r) {
-    for (int filter_c = -filterWidth/2; filter_c <= filterWidth/2; ++filter_c) {
+  for (int filter_r =  -filterWidth/2; filter_r <= filterWidth/2; ++filter_r) {
+    for (int filter_c =  -filterWidth/2; filter_c <= filterWidth/2; ++filter_c) {
+      /*
       int image_r = 0, image_c = 0;
       if(absolute_image_position.y + filter_r < 0) {
         image_r = 0;
@@ -150,8 +151,9 @@ void gaussian_blur(const unsigned char* const inputChannel,
           image_c = absolute_image_position.x + filter_c;
         }
       }
-      //int image_r = std::min(std::max(absolute_image_position.y + filter_r, 0), static_cast<int>(numRows - 1));
-      //int image_c = std::min(std::max(absolute_image_position.x + filter_c, 0), static_cast<int>(numCols - 1));
+      */
+      int image_r = min(max(absolute_image_position.y + filter_r, 0), static_cast<int>(numRows - 1));
+      int image_c = min(max(absolute_image_position.x + filter_c, 0), static_cast<int>(numCols - 1));
       float image_value = static_cast<float>(inputChannel[image_r * numCols + image_c]);
       float filter_value = filter[(filter_r + filterWidth/2) * filterWidth + filter_c + filterWidth/2];
       result += image_value * filter_value;
